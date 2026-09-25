45fd517ac361eaf4f921c5aa9d6dfba2b22b2df6
skpl57 <kania.skpl57@gmail.com>
2026-09-25 10:11:47 +0200

    zaleznosci retrofit'a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
 }
